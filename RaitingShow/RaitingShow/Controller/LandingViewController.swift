e716f8c325bdce311844d7354581e6626cbba95e
GhaziShubayli <ghazi.shubayli.jiz.s@tuwaiq.edu.sa>
2022-01-06 16:13:03 +0300

    "adding EN & AR button"

--- RaitingShow/RaitingShow/Controller/LandingViewController.swift
+++ RaitingShow/RaitingShow/Controller/LandingViewController.swift
@@ -8,7 +8,35 @@
 import UIKit
 
 class LandingViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var languageSegmentControl: UISegmentedControl! {
+        didSet {
+            if let language = UserDefaults.standard.string(forKey: "currentLanguage") {
+                switch language {
+                case "ar":
+                    languageSegmentControl.selectedSegmentIndex = 0
+                case "en":
+                    languageSegmentControl.selectedSegmentIndex = 1
+                default:
+                    let localLanguage =  Locale.current.languageCode
+                    if localLanguage == "ar" {
+                        languageSegmentControl.selectedSegmentIndex = 0
+                    }else {
+                        languageSegmentControl.selectedSegmentIndex = 1
+                    }
+                }
+            }else {
+                let localLanguage =  Locale.current.languageCode
+                if localLanguage == "ar" {
+                    languageSegmentControl.selectedSegmentIndex = 0
+                }else {
+                    languageSegmentControl.selectedSegmentIndex = 1
+                }
+            }
+        }
+    }
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var regesterButton: UIButton!
     override func viewDidLoad() {
@@ -17,8 +45,16 @@ class LandingViewController: UIViewController {
         regesterButton.setTitle("regester".localized, for: .normal)
         // Do any additional setup after loading the view.
     }
-    
-
+    @IBAction func languageChangerPressed(_ sender: UISegmentedControl) {
+        if let language = sender.titleForSegment(at: sender.selectedSegmentIndex)?.lowercased() {
+            UserDefaults.standard.set(language, forKey: "currentLanguage")
+            Bundle.setLanguage(language)
+            let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let sceneDelegate = windowScene.delegate as? SceneDelegate {
+                sceneDelegate.window?.rootViewController = storyboard.instantiateInitialViewController()
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
